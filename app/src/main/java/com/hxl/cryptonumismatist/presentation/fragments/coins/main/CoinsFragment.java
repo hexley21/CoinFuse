8a2051041661c952ba9b107e97f039f4df20cf00
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-09-23 21:53:49 +0400

    feat(presentation): add fragments to nav graph
    Add "Welcome", "Coin" and "Coin Details" fragment classes and layouts to app. Insert listed fragments to navigation graph.
    Add "Data-Binding" build features to gradle

---
+++ app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
@@ -0,0 +1,21 @@
+package com.hxl.cryptonumismatist.presentation.fragments.coins.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hxl.cryptonumismatist.R;
+
+public class CoinsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coins, container, false);
+    }
+}
