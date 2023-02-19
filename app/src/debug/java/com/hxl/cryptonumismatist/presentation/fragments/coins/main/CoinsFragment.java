4b0c3312273759764fdae8950d974034fd47fa2d
hexley21 <a.kupreishvili.21@gmail.com>
2023-02-19 21:59:39 +0400

    feat(debug): add lifecycle log messages

--- app/src/debug/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
+++ app/src/debug/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
@@ -1,6 +1,11 @@
 package com.hxl.cryptonumismatist.presentation.fragments.coins.main;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,16 +13,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.hxl.cryptonumismatist.R;
 import com.hxl.cryptonumismatist.databinding.FragmentCoinsBinding;
 import com.hxl.cryptonumismatist.presentation.fragments.welcome.WelcomeFragmentViewModel;
 
 public class CoinsFragment extends Fragment {
+    private static final String TAG = "Coins";
     private WelcomeFragmentViewModel vmWelcome;
     private FragmentCoinsBinding binding;
 
@@ -26,52 +27,75 @@ public class CoinsFragment extends Fragment {
         vmWelcome = new ViewModelProvider(requireActivity()).get(WelcomeFragmentViewModel.class);
         binding = FragmentCoinsBinding.inflate(inflater, container, false);
 
-        Log.d("CN_Coins", "onCreateView");
+        Log.d(TAG, "onCreateView");
         return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated");
+        binding.btnBack.setOnClickListener(v -> {
+            vmWelcome.saveWelcome.invoke(true);
+            NavHostFragment.findNavController(this).navigate(R.id.action_coinsFragment_to_welcomeFragment);
+            Log.d(TAG, "btnBack");
+        });
+
     }
 
     // region lifecycle
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
-        Log.d("CN_Coins", "onStart");
+        Log.d(TAG, "onStart");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d("CN_Coins", "onResume");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d("CN_Coins", "onPause");
+        Log.d(TAG, "onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.d("CN_Coins", "OnStop");
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("CN_Coins", "OnDestroy");
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach");
     }
     // endregion
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.btnBack.setOnClickListener(v -> {
-            vmWelcome.saveWelcome.invoke(true);
-            NavHostFragment.findNavController(this).navigate(R.id.action_coinsFragment_to_welcomeFragment);
-            Log.d("CN_Coins", "btnBack");
-        });
-
-        Log.d("CN_Coins", "onViewCreated");
-    }
 }
--- app/src/debug/java/com/hxl/cryptonumismatist/presentation/fragments/welcome/WelcomeFragment.java
+++ app/src/debug/java/com/hxl/cryptonumismatist/presentation/fragments/welcome/WelcomeFragment.java
@@ -1,5 +1,6 @@
 package com.hxl.cryptonumismatist.presentation.fragments.welcome;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
@@ -21,25 +23,27 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class WelcomeFragment extends Fragment {
 
+    private static final String TAG = "Welcome";
     private FragmentWelcomeBinding binding;
     private WelcomeFragmentViewModel vm;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         binding = FragmentWelcomeBinding.inflate(inflater, container, false);
         vm = new ViewModelProvider(this).get(WelcomeFragmentViewModel.class);
         if (!vm.getWelcome.invoke()) {
             openCoinsFragment();
         }
-        Log.d("CN_Welcome", String.format("getWelcome : %b", vm.getWelcome.invoke()));
-        Log.d("CN_Welcome", "onCreateView");
+        Log.d(TAG, String.format("getWelcome : %b", vm.getWelcome.invoke()));
+
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        Log.d(TAG, "onViewCreated");
         ViewPager2 walkThroughPager = binding.walkThroughPager;
         walkThroughPager.setAdapter(new WalkThroughPagerAdapter(this, WalkThroughEnums.values()));
         walkThroughPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
@@ -51,7 +55,7 @@ public class WelcomeFragment extends Fragment {
                 } else if (binding.btnGetStarted.getVisibility() == View.VISIBLE) {
                     loadLastPage(false);
                 }
-                Log.d("CN_Welcome", String.format("pagerItem: %d", walkThroughPager.getCurrentItem()));
+                Log.d(TAG, String.format("pagerItem: %d", walkThroughPager.getCurrentItem()));
             }
         });
 
@@ -69,44 +73,12 @@ public class WelcomeFragment extends Fragment {
 
         binding.btnGetStarted.setOnClickListener(exitButtonClick);
         binding.tvSkip.setOnClickListener(exitButtonClick);
-        Log.d("CN_Welcome", "onViewCreated");
-    }
 
-    // region lifecycle
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d("CN_Welcome", "onStart");
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d("CN_Welcome", "onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d("CN_Welcome", "onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d("CN_Welcome", "OnStop");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d("CN_Welcome", "OnDestroy");
-    }
-    // endregion
 
     private void openCoinsFragment() {
         NavHostFragment.findNavController(this).navigate(R.id.action_welcomeFragment_to_coinsFragment);
-        Log.d("CN_Welcome", "openCoinsFragment");
+        Log.d(TAG, "openCoinsFragment");
     }
 
     private void loadLastPage(@NonNull Boolean visibility) {
@@ -139,6 +111,63 @@ public class WelcomeFragment extends Fragment {
 
         binding.tabIndicator.setVisibility(vElse);
         binding.tabIndicator.setAnimation(AnimationUtils.loadAnimation(requireContext(), aElse));
-        Log.d("CN_Welcome", String.format("loadLastPage(%b)", visibility));
+        Log.d(TAG, String.format("loadLastPage(%b)", visibility));
     }
+
+    // region lifecycle
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach");
+    }
+    // endregion
+
 }
