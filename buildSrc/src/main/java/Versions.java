c614d6aee4417a68b5d0122f678aca870b0766b7
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-10 18:26:35 +0400

    build(dependencies): add versions class

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,34 @@
+public class Versions {
+    /* Core */
+    public static final String appCompatVersion = "1.6.1";
+    public static final String materialVersion = "1.8.0";
+    public static final String constraintLayoutVersion = "2.1.4";
+    public static final String splashScreenVersion = "1.0.0";
+    // Fragment
+    public static final String fragmentVersion = "1.5.5";
+    // Navigation
+    public static final String navVersion = "2.5.3";
+
+    //UI
+    public static final String circleIndicatorVersion = "2.1.6";
+
+    // LifeCycle
+    public static final String lifecycleVersion = "2.6.0-alpha04";
+
+    // Hilt
+    public static final String hiltVersion = "2.45";
+
+    // Reactive
+    public static final String rxJavaVersion = "3.1.6";
+    public static final String rxAndroidVersion = "3.0.2";
+
+    // Retrofit
+    public static final String retrofitVersion = "2.9.0";
+
+    /* Test */
+    // JUnit
+    public static final String junitVersion = "4.13.2";
+    public static final String extJunitVersion = "1.1.5";
+    // espresso
+    public static final String espressoVersion = "3.5.1";
+}
