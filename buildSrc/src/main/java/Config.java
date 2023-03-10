f8ab94f616421ee66e96263076fab7d35ff269cd
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-10 18:25:49 +0400

    build(dependencies): add config class

---
+++ buildSrc/src/main/java/Config.java
@@ -0,0 +1,14 @@
+public final class Config {
+    public static final class Android {
+        public static final int minSdk = 21;
+        public static final int targetSdk = 33;
+        public static final int compileSdk = 33;
+    }
+
+    public static final class Plugins {
+        public static final String android = "com.android.application";
+        public static final String androidLibrary = "com.android.library";
+        public static final String javaLibrary = "java-library";
+        public static final String dagger = "com.google.dagger.hilt.android";
+    }
+}
