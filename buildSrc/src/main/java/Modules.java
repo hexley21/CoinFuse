30261496a1187d9586b08869c70dcae46004dd6a
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-10 18:26:08 +0400

    build(dependencies): add module class

---
+++ buildSrc/src/main/java/Modules.java
@@ -0,0 +1,12 @@
+public final class Modules {
+    public static final String app = ":app";
+    public static final String domain = ":domain";
+    public static final String data = ":data";
+    public static final String remote = ":remote";
+    public static final String local = ":local";
+
+    public final static class NameSpaces {
+        public static final String app = "com.hxl.cryptonumismatist";
+        public static final String local = "com.hxl.local";
+    }
+}
