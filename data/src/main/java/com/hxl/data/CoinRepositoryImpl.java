4858842df6b5e95f36063dcfc86a2eb98762673c
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-14 22:28:46 +0400

    feat(coins): implement remote coin source into coin repository

--- data/src/main/java/com/hxl/data/CoinRepositoryImpl.java
+++ data/src/main/java/com/hxl/data/CoinRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.hxl.data;
 
+import com.hxl.data.repository.coin.CoinRemote;
 import com.hxl.domain.model.Coin;
 import com.hxl.domain.repository.CoinRepository;
 
@@ -8,44 +9,50 @@ import java.util.List;
 import io.reactivex.rxjava3.core.Single;
 
 public class CoinRepositoryImpl implements CoinRepository {
+
+    private final CoinRemote remoteSource;
+
+    public CoinRepositoryImpl(CoinRemote source) {
+        this.remoteSource = source;
+    }
     @Override
     public Single<List<Coin>> getCoins() {
-        return null;
+        return remoteSource.getCoins();
     }
 
     @Override
     public Single<List<Coin>> getCoins(int limit, int offset) {
-        return null;
+        return remoteSource.getCoins(limit, offset);
     }
 
     @Override
     public Single<List<Coin>> getCoins(String ids) {
-        return null;
+        return remoteSource.getCoins(ids);
     }
 
     @Override
     public Single<Coin> getCoin(String id) {
-        return null;
+        return remoteSource.getCoin(id);
     }
 
     @Override
     public void saveCoins(List<Coin> coins) {
-
+        throw new UnsupportedOperationException("saveCoins is not supported yet");
     }
 
     @Override
     public void bookMarkCoin(String id) {
-
+        throw new UnsupportedOperationException("bookMarkCoin is not supported yet");
     }
 
     @Override
     public void unBookMarkCoin(String id) {
-
+        throw new UnsupportedOperationException("unBookMarkCoin is not supported yet");
     }
 
     @Override
     public Single<List<Coin>> getBookMarkedCoins() {
-        return null;
+        throw new UnsupportedOperationException("getBookMarkedCoins is not supported yet");
     }
 
 }
