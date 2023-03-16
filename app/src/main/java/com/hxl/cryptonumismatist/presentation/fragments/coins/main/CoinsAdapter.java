8313f24e2e0d33e32928b081fa23fd79b2044e2c
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-16 22:49:17 +0400

    feat(coins/ui): update coins fragment add coins adapter & add coin view model

---
+++ app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsAdapter.java
@@ -0,0 +1,62 @@
+package com.hxl.cryptonumismatist.presentation.fragments.coins.main;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.AsyncListDiffer;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.RequestManager;
+import com.hxl.cryptonumismatist.databinding.CoinItemBinding;
+import com.hxl.cryptonumismatist.presentation.fragments.BaseAdapter;
+import com.hxl.domain.model.Coin;
+
+import javax.inject.Inject;
+
+public class CoinsAdapter extends BaseAdapter<Coin> {
+    private final RequestManager glide;
+
+    @Inject
+    public CoinsAdapter(RequestManager glide) {
+        this.glide = glide;
+        DiffUtil.ItemCallback<Coin> diffCallBack = new DiffUtil.ItemCallback<Coin>() {
+            @Override
+            public boolean areItemsTheSame(@NonNull Coin oldItem, @NonNull Coin newItem) {
+                return oldItem.id.equals(newItem.id);
+            }
+
+            @Override
+            public boolean areContentsTheSame(@NonNull Coin oldItem, @NonNull Coin newItem) {
+                return oldItem.equals(newItem);
+            }
+        };
+        CoinsAdapter.super.differ = new AsyncListDiffer<>(this, diffCallBack);
+    }
+
+    @Override
+    protected RecyclerView.ViewHolder getViewHolder(ViewGroup parent, int viewType) {
+        CoinItemBinding binding = CoinItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new CoinViewHolder(binding);
+    }
+
+    private class CoinViewHolder extends RecyclerView.ViewHolder implements Binder<Coin> {
+        CoinItemBinding binding;
+        public CoinViewHolder(CoinItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        @SuppressLint("DefaultLocale")
+        @Override
+        public void bind(Coin item) {
+            binding.setName(item.name);
+            binding.setSymbol(item.symbol);
+            binding.setPrice(String.format("%,.2f$", item.priceUsd));
+            binding.setChange(String.format("%.2f%%", item.changePercent24Hr));
+            glide.load(item.img).into(binding.imgCoin);
+        }
+    }
+}
--- app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
+++ app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
@@ -6,17 +6,53 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.hxl.cryptonumismatist.databinding.FragmentCoinsBinding;
+import com.hxl.cryptonumismatist.presentation.fragments.BaseFragment;
 
-public class CoinsFragment extends Fragment {
-    private FragmentCoinsBinding binding;
+import javax.inject.Inject;
 
+import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+@AndroidEntryPoint
+public class CoinsFragment extends BaseFragment<FragmentCoinsBinding, CoinsFragmentViewModel> {
+
+    private final CompositeDisposable disposable = new CompositeDisposable();
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentCoinsBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+    protected FragmentCoinsBinding setViewBinding(LayoutInflater inflater, ViewGroup container) {
+        return FragmentCoinsBinding.inflate(inflater, container, false);
     }
 
+    @Override
+    protected CoinsFragmentViewModel setViewModel() {
+        return new ViewModelProvider(this).get(CoinsFragmentViewModel.class);
+    }
+
+    @Inject
+    CoinsAdapter coinsAdapter;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        RecyclerView coinsRv = binding.rvCoins;
+        coinsRv.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        disposable.add(
+                vm.getCoins()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(coins -> {
+                            coinsAdapter.setList(coins);
+                            coinsRv.setAdapter(coinsAdapter);
+                        })
+        );
+
+
+    }
 }
