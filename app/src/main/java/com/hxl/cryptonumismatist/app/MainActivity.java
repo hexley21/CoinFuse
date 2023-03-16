f756c47a8b68eb30e58ddf5dff189b7856a80ca4
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-16 21:56:09 +0400

    feat(debugging): remove unused callback logs from main activity

--- app/src/main/java/com/hxl/cryptonumismatist/app/MainActivity.java
+++ app/src/main/java/com/hxl/cryptonumismatist/app/MainActivity.java
@@ -3,7 +3,6 @@ package com.hxl.cryptonumismatist.app;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
@@ -21,57 +20,5 @@ public class MainActivity extends AppCompatActivity {
 
         SplashScreen.installSplashScreen(this).setKeepOnScreenCondition(() -> false );
         setContentView(R.layout.activity_main);
-
     }
-
-    // region lifecycle
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.d(TAG, "onRestoreInstanceState");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.d(TAG, "onSaveInstanceState");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-    // endregion
-
 }
