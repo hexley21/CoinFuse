41fc379bf483df71e36ce0d01db5a53ed4e30859
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-25 23:23:23 +0400

    test(coins): add test constants class

---
+++ remote/src/test/java/com/hxl/remote/fake/TestConstants.java
@@ -0,0 +1,12 @@
+package com.hxl.remote.fake;
+
+public class TestConstants {
+    public static final String ASSET_URL = "https://assets/%s.png";
+    public static final String ID = "bitcoin";
+    public static final int SIZE = 5;
+    public static final int LIMIT = 3;
+    public static final int OFFSET = 12;
+    public static final long TIMESTAMP = 8L;
+
+
+}
