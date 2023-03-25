5482d52c850d49fcdce61072ccd69a2dc1a5dc78
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-25 23:27:43 +0400

    test(coins): move random generation logic from mapper to constants

--- remote/src/test/java/com/hxl/remote/fake/TestConstants.java
+++ remote/src/test/java/com/hxl/remote/fake/TestConstants.java
@@ -1,5 +1,8 @@
 package com.hxl.remote.fake;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class TestConstants {
     public static final String ASSET_URL = "https://assets/%s.png";
     public static final String ID = "bitcoin";
@@ -8,5 +11,18 @@ public class TestConstants {
     public static final int OFFSET = 12;
     public static final long TIMESTAMP = 8L;
 
+    private static final Random random = new Random();
+    public static String randomName() {
+        return UUID.randomUUID().toString();
+    }
+    public static String randomInt() {
+        return String.valueOf(random.nextInt());
+    }
+    public static String randomDouble() {
+        return String.valueOf(random.nextDouble());
+    }
+    public static String randomFloat() {
+        return String.valueOf(random.nextFloat());
+    }
 
 }
