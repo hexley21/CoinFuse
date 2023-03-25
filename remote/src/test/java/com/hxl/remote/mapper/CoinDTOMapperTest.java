4d488e5437a5074b4845d61ac018ce8711bef01d
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-25 19:13:17 +0400

    test(coins): add test for coin dto mapper

--- remote/src/test/java/com/hxl/remote/mapper/CoinDTOMapperTest.java
+++ remote/src/test/java/com/hxl/remote/mapper/CoinDTOMapperTest.java
@@ -1,33 +1,77 @@
 package com.hxl.remote.mapper;
 
 import com.hxl.domain.model.Coin;
-import com.hxl.remote.fake.FakeRemoteData;
 import com.hxl.remote.model.CoinDTO;
 import com.hxl.remote.model.CoinDTOMapper;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CoinDTOMapperTest {
 
-    String assetUrl = "https://source/%s.img";
     CoinDTOMapper mapper;
+    Long timestamp;
 
     @Before
     public void setUp() {
-        mapper = new CoinDTOMapper(assetUrl);
+        mapper = new CoinDTOMapper("https://assets/%s.png");
+        timestamp = System.currentTimeMillis();
     }
 
     @Test
     public void mapDtoToModelReturnsConvertedObject() {
         // Arrange
-        CoinDTO dto = new FakeRemoteData().getFakeCoin();
+        CoinDTO dto = new CoinDTO(
+                "bitcoin",
+                "1",
+                "BTC",
+                "Bitcoin",
+                "1D",
+                "2D",
+                "3D",
+                "4D",
+                "5D",
+                "6F",
+                "7D",
+                "bitcoin.com"
+        );
+        Coin coin = new Coin(
+                "bitcoin",
+                1,
+                "BTC",
+                "Bitcoin",
+                1D,
+                2D,
+                3D,
+                4D,
+                5D,
+                6F,
+                7D,
+                "bitcoin.com",
+                timestamp,
+                "https://assets/btc.png"
+        );
         // Act
-        Coin coin = mapper.mapFromDTO(dto, System.currentTimeMillis());
+        Coin result = mapper.mapFromDTO(dto, timestamp);
         // Assert
-        assertNotNull(coin);
-        assertEquals(coin.img, String.format(assetUrl, dto.symbol.toLowerCase()));
+        assertEquals(result.id, coin.id);
+        assertEquals(result.rank, coin.rank);
+        assertEquals(result.symbol, coin.symbol);
+        assertEquals(result.name, coin.name);
+        assertEquals(result.supply, coin.supply);
+        assertEquals(result.maxSupply, coin.maxSupply);
+        assertEquals(result.marketCapUsd, coin.marketCapUsd);
+        assertEquals(result.volumeUsd24Hr, coin.volumeUsd24Hr);
+        assertEquals(result.priceUsd, coin.priceUsd);
+        assertEquals(result.changePercent24Hr, coin.changePercent24Hr);
+        assertEquals(result.vwap24Hr, coin.vwap24Hr);
+        assertEquals(result.explorer, coin.explorer);
+        assertEquals(result.timestamp, coin.timestamp);
+        assertEquals(result.img, coin.img);
     }
 }
