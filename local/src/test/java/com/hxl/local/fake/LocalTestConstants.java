42a1d3847ffb0bd6e9764957202cb9d990dedb7c
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-26 19:35:27 +0400

    test(coins): update local module test constants

--- local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
+++ local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
@@ -6,8 +6,7 @@ import java.util.UUID;
 public class LocalTestConstants {
     public static final String ASSET_URL = "https://assets/%s.png";
     public static final String ID = "bitcoin";
-    public static final String KEY = "bitcoin,ethereum";
-    public static final int KEY_RESPONSE_SIZE = KEY.split(",").length;
+    public static final String[] KEYS = new String[] {"bitcoin", "ethereum"};
     public static final int SIZE = 5;
     public static final long TIMESTAMP = 8L;
 
