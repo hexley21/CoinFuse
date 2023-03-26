0764d128290fa78aec97b5e3dc2dae87b5f2156b
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-26 20:12:36 +0400

    test(coins): add each get/search coin tests for coin local implementation & update test constants
    Add test for searchCoins(String key)
    Add test for getCoins(List<String> ids)
    Add test for getCoins(int limit, int offset)
    Add test for getCoins()
    Update fields of constants

--- local/src/test/java/com/hxl/local/CoinLocalImplTest.java
+++ local/src/test/java/com/hxl/local/CoinLocalImplTest.java
@@ -6,15 +6,25 @@ import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.hxl.domain.model.Coin;
 import com.hxl.local.database.BookmarkDao;
 import com.hxl.local.database.CoinDao;
 import com.hxl.local.database.CoinDatabase;
+import com.hxl.local.fake.FakeLocalDataFactory;
+import com.hxl.local.model.CoinEntity;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
+import static com.hxl.local.fake.LocalTestConstants.*;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
 
 @Config(manifest = Config.NONE)
 @RunWith(AndroidJUnit4.class)
@@ -41,4 +51,115 @@ public class CoinLocalImplTest {
         database.close();
     }
 
+    @Test
+    public void get_coins_returns_data_from_database() {
+        // Arrange
+        List<CoinEntity> entities = FakeLocalDataFactory.getFakeData(KEYS);
+        // Act
+        Completable saveCoins = coinDao.addCoin(entities.toArray(new CoinEntity[0]));
+        Single<List<Coin>> getCoins = coinSource.getCoins();
+        // Assert
+        saveCoins.test()
+                .assertNoErrors()
+                .assertComplete();
+        getCoins.test()
+                .awaitCount(1)
+                .assertNoErrors()
+                .assertValue(d -> {
+                    for (int i = 0; i < d.size(); i++) {
+                        if (!d.get(i).id.equals(KEYS[i])){
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+    }
+
+    @Test
+    public void get_coins_by_limit_and_offset_returns_data_from_database() {
+        // Arrange
+        List<CoinEntity> entities = FakeLocalDataFactory.getFakeData(KEYS);
+        // Act
+        Completable saveCoins = coinDao.addCoin(entities.toArray(new CoinEntity[0]));
+        Single<List<Coin>> getCoins = coinSource.getCoins(LIMIT, OFFSET);
+        // Assert
+        saveCoins.test()
+                .assertNoErrors()
+                .assertComplete();
+        getCoins.test()
+                .awaitCount(1)
+                .assertNoErrors()
+                .assertValue(d -> {
+                    for (int i = 0; i < LIMIT; i++) {
+                        if (!d.get(i).id.equals(KEYS[i+OFFSET])){
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+    }
+
+    @Test
+    public void get_coins_by_ids_returns_data_from_database() {
+        // Arrange
+        List<CoinEntity> entities = FakeLocalDataFactory.getFakeData(KEYS);
+        // Act
+        Completable saveCoins = coinDao.addCoin(entities.toArray(new CoinEntity[0]));
+        Single<List<Coin>> getCoins = coinSource.getCoins(IDS);
+        // Assert
+        saveCoins.test()
+                .assertNoErrors()
+                .assertComplete();
+        getCoins.test()
+                .awaitCount(1)
+                .assertNoErrors()
+                .assertValue(d -> {
+                    for (int i = 0; i < IDS.size(); i++) {
+                        if (!d.get(i).id.equals(IDS.get(i))){
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+    }
+    @Test
+    public void search_coins_returns_data_from_database() {
+        // Arrange
+        List<CoinEntity> entities = FakeLocalDataFactory.getFakeData(KEYS);
+        // Act
+        Completable saveCoins = coinDao.addCoin(entities.toArray(new CoinEntity[0]));
+        Single<List<Coin>> getCoins = coinSource.searchCoins(ID);
+        // Assert
+        saveCoins.test()
+                .assertNoErrors()
+                .assertComplete();
+        getCoins.test()
+                .awaitCount(1)
+                .assertNoErrors()
+                .assertValue(d -> {
+                    for (int i = 0; i < d.size(); i++) {
+                        if (!d.get(i).id.equals(KEYS[i])) {
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+    }
+
+    @Test
+    public void get_coin_by_id_returns_data_from_database() {
+        // Arrange
+        List<CoinEntity> entities = FakeLocalDataFactory.getFakeData(KEYS);
+        // Act
+        Completable saveCoins = coinDao.addCoin(entities.toArray(new CoinEntity[0]));
+        Single<Coin> getCoins = coinSource.getCoin(ID);
+        // Assert
+        saveCoins.test()
+                .assertNoErrors()
+                .assertComplete();
+        getCoins.test()
+                .awaitCount(1)
+                .assertNoErrors()
+                .assertValue(d -> d.id.equals(ID));
+    }
 }
--- local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
+++ local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
@@ -1,16 +1,20 @@
 package com.hxl.local.fake;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class LocalTestConstants {
-    public static final String ASSET_URL = "https://assets/%s.png";
-    public static final String ID = "bitcoin";
-    public static final String[] KEYS = new String[] {"bitcoin", "ethereum", "monero", "cardano", "solana"};
-    public static final int SIZE = 5;
     public static final int LIMIT = 3;
     public static final int OFFSET = 2;
     public static final long TIMESTAMP = 8L;
+    public static final String ASSET_URL = "https://assets/%s.png";
+    public static final String ID = "bitcoin";
+    public static final String[] KEYS = new String[] {"bitcoin", "bitcoin-gold", "ethereum", "monero", "cardano", "solana"};
+    public static List<String> IDS = Arrays.stream(Arrays.copyOfRange(KEYS, OFFSET, LIMIT)).collect(Collectors.toList());
 
 
     private static final Random random = new Random();
