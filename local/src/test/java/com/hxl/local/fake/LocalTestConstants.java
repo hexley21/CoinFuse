f7366326b8d9cf89240d2ef9ffb3a51af377c78f
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-26 20:07:45 +0400

    test(coins): update local test constants
    Add OFFSET and LIMIT fields
    Update KEYS field

--- local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
+++ local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
@@ -6,8 +6,10 @@ import java.util.UUID;
 public class LocalTestConstants {
     public static final String ASSET_URL = "https://assets/%s.png";
     public static final String ID = "bitcoin";
-    public static final String[] KEYS = new String[] {"bitcoin", "ethereum"};
+    public static final String[] KEYS = new String[] {"bitcoin", "ethereum", "monero", "cardano", "solana"};
     public static final int SIZE = 5;
+    public static final int LIMIT = 3;
+    public static final int OFFSET = 2;
     public static final long TIMESTAMP = 8L;
 
 
