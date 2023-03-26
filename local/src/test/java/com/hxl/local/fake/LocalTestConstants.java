dcc594f015c01d2161c43480fd2b7e8a1aacf454
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-26 18:16:58 +0400

    test(coins): add test constants to local module

---
+++ local/src/test/java/com/hxl/local/fake/LocalTestConstants.java
@@ -0,0 +1,10 @@
+package com.hxl.local.fake;
+
+public class LocalTestConstants {
+    public static final String ASSET_URL = "https://assets/%s.png";
+    public static final String ID = "bitcoin";
+    public static final String KEY = "bitcoin,ethereum";
+    public static final int KEY_RESPONSE_SIZE = KEY.split(",").length;
+    public static final int SIZE = 5;
+    public static final long TIMESTAMP = 8L;
+}
