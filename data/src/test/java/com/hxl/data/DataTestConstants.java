06505092584c909a3215e0014a04817bdc35b5b4
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-28 21:43:15 +0400

    test(prefs): add get method tests for preference repository implementation

---
+++ data/src/test/java/com/hxl/data/DataTestConstants.java
@@ -0,0 +1,16 @@
+package com.hxl.data;
+
+public class DataTestConstants {
+    // Keys
+    public static final String KEY_INT = "int";
+    public static final String KEY_BOOL = "bool";
+    public static final String KEY_STR = "str";
+    // Values
+    public static final int VAL_INT = 123;
+    public static final boolean VAL_BOOL = true;
+    public static final String VAL_STR = "test-str";
+    // Default values
+    public static final int DEF_INT = 321;
+    public static final boolean DEF_BOOL = false;
+    public static final String DEF_STR = "default";
+}
---
+++ data/src/test/java/com/hxl/data/PreferenceRepositoryImplTest.java
@@ -0,0 +1,64 @@
+package com.hxl.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static com.hxl.data.DataTestConstants.*;
+
+import com.hxl.data.repository.pref.PreferenceLocal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PreferenceRepositoryImplTest {
+
+
+    @Mock
+    PreferenceLocal preferenceLocal;
+
+    @InjectMocks
+    PreferenceRepositoryImpl repository;
+
+    @Test
+    public void get_int_returns_value() {
+        // Arrange
+        when(preferenceLocal.get(anyString(), anyInt())).thenReturn(VAL_INT);
+        // Act
+        int intPref = repository.get(KEY_INT, DEF_INT);
+        // Assert
+        assertEquals(VAL_INT, intPref);
+        verify(preferenceLocal, times(1)).get(anyString(), anyInt());
+    }
+
+    @Test
+    public void get_bool_returns_value() {
+        // Arrange
+        when(preferenceLocal.get(anyString(), anyBoolean())).thenReturn(VAL_BOOL);
+        // Act
+        boolean boolPref = repository.get(KEY_BOOL, DEF_BOOL);
+        // Assert
+        assertEquals(VAL_BOOL, boolPref);
+        verify(preferenceLocal, times(1)).get(anyString(), anyBoolean());
+    }
+
+    @Test
+    public void get_string_returns_value() {
+        // Arrange
+        when(preferenceLocal.get(anyString(), anyString())).thenReturn(VAL_STR);
+        // Act
+        String strPref = repository.get(KEY_STR, DEF_STR);
+        // Assert
+        assertEquals(VAL_STR, strPref);
+        verify(preferenceLocal, times(1)).get(anyString(), anyString());
+    }
+}
