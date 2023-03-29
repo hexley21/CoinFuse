1722337d9c2943fef6085aa9020bacace3a0f96f
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-29 16:14:04 +0400

    test(coins): add bookmark coin interactor test

---
+++ domain/src/test/java/com/hxl/interactors/coins/BookmarkCoinTest.java
@@ -0,0 +1,40 @@
+package com.hxl.interactors.coins;
+
+import static com.hxl.interactors.fakes.DomainTestConstants.ID;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.hxl.domain.interactors.coins.BookmarkCoin;
+import com.hxl.domain.repository.CoinRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.reactivex.rxjava3.core.Completable;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookmarkCoinTest {
+
+    @Mock
+    CoinRepository repository;
+    @InjectMocks
+    BookmarkCoin interactor;
+
+    @Test
+    public void invokeInsertsCoinIdToRepository() {
+        // Arrange
+        when(repository.bookmarkCoin(anyString())).thenReturn(Completable.complete());
+        // Act
+        Completable bookmarkCoin = interactor.invoke(ID);
+        // Assert
+        bookmarkCoin.test()
+                .assertComplete()
+                .assertNoErrors();
+        verify(repository).bookmarkCoin(anyString());
+    }
+}
