663f2b9655e15ed52efb5145ecab2d9f1d5e784f
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-29 15:50:27 +0400

    test(coins): add domain test constants

---
+++ domain/src/test/java/com/hxl/interactors/fakes/DomainTestConstants.java
@@ -0,0 +1,7 @@
+package com.hxl.interactors.fakes;
+
+public class DomainTestConstants {
+    public static final int SIZE = 5;
+    public static final int LIMIT = 2;
+    public static final int OFFSET = 3;
+}
