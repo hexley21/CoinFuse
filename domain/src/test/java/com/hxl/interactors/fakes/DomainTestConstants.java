f052beeab6b98b1cbbb2d8e355d590d611180db5
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-29 16:01:45 +0400

    test(coins): add get coin interactor test

--- domain/src/test/java/com/hxl/interactors/fakes/DomainTestConstants.java
+++ domain/src/test/java/com/hxl/interactors/fakes/DomainTestConstants.java
@@ -1,6 +1,7 @@
 package com.hxl.interactors.fakes;
 
 public class DomainTestConstants {
+    public static final String ID = "bitcoin";
     public static final int SIZE = 5;
     public static final int LIMIT = 2;
     public static final int OFFSET = 3;
