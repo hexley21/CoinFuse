ad1ae74f5f8618d91cdbd12ee8b872c29361a7e3
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-30 19:35:20 +0400

    feat(coins): add temporary search feature to coins fragment

--- app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
+++ app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragment.java
@@ -1,6 +1,8 @@
 package com.hxl.cryptonumismatist.presentation.fragments.coins.main;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,17 +16,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.hxl.cryptonumismatist.databinding.FragmentCoinsBinding;
 import com.hxl.cryptonumismatist.presentation.fragments.BaseFragment;
+import com.hxl.domain.model.Coin;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 @AndroidEntryPoint
 public class CoinsFragment extends BaseFragment<FragmentCoinsBinding, CoinsFragmentViewModel> {
 
-    private final CompositeDisposable disposable = new CompositeDisposable();
+//    private final CompositeDisposable disposable = new CompositeDisposable();
     @Override
     protected FragmentCoinsBinding setViewBinding(LayoutInflater inflater, ViewGroup container) {
         return FragmentCoinsBinding.inflate(inflater, container, false);
@@ -42,14 +48,58 @@ public class CoinsFragment extends BaseFragment<FragmentCoinsBinding, CoinsFragm
 
         RecyclerView coinsRv = binding.rvCoins;
         coinsRv.setLayoutManager(new LinearLayoutManager(requireContext()));
+        coinsRv.setAdapter(coinsAdapter);
+        if (binding.tfSearch.getEditText() != null) {
+            binding.tfSearch.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        disposable.add(
-                vm.getCoins()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(coins -> {
-                            coinsAdapter.setList(coins);
-                            coinsRv.setAdapter(coinsAdapter);
-                        }, error -> Log.e("CoinsFragment", error.getMessage(), error)
-                        ));
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    vm.searchCoins(s.toString())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(new SingleObserver<List<Coin>>() {
+
+                                @Override
+                                public void onSubscribe(Disposable d) {
+                                }
+
+                                @Override
+                                public void onSuccess(List<Coin> coins) {
+                                    coinsAdapter.setList(coins);
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+                                }
+                            });
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+        }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        vm.getCoins()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<Coin>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) { }
+                    @Override
+                    public void onSuccess(List<Coin> coins) {
+                        coinsAdapter.setList(coins);
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("CoinsFragment", e.getMessage(), e);
+                    }
+                });
     }
 }
--- app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragmentViewModel.java
+++ app/src/main/java/com/hxl/cryptonumismatist/presentation/fragments/coins/main/CoinsFragmentViewModel.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
 import com.hxl.domain.interactors.coins.GetCoins;
+import com.hxl.domain.interactors.coins.SearchCoins;
 import com.hxl.domain.model.Coin;
 
 import org.jetbrains.annotations.NotNull;
@@ -19,13 +20,19 @@ import io.reactivex.rxjava3.core.Single;
 public class CoinsFragmentViewModel extends ViewModel {
 
     @NotNull private final GetCoins getCoins;
+    @NotNull private final SearchCoins searchCoins;
 
     public Single<List<Coin>> getCoins() {
         return getCoins.invoke();
     }
 
+    public Single<List<Coin>> searchCoins(String key) {
+        return searchCoins.invoke(key);
+    }
+
     @Inject
-    public CoinsFragmentViewModel(@NonNull GetCoins getCoins) {
+    public CoinsFragmentViewModel(@NonNull GetCoins getCoins, @NonNull SearchCoins searchCoins) {
         this.getCoins = getCoins;
+        this.searchCoins = searchCoins;
     }
 }
