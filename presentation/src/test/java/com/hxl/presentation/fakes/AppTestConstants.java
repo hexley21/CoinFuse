94923249867964ab6ab18abe242bddcb13070d3d
hexley21 <a.kupreishvili.21@gmail.com>
2023-03-30 21:22:25 +0400

    test(coins): add base tests for coins menu & welcome view-model

--- presentation/src/test/java/com/hxl/presentation/fakes/AppTestConstants.java
+++ presentation/src/test/java/com/hxl/presentation/fakes/AppTestConstants.java
@@ -7,6 +7,16 @@ public class AppTestConstants {
     // Coins
     public static final int SIZE = 5;
     public static final String ID = "bitcoin";
+    // Prefs
+
+    // Defaults
+    public static final int DEF_INT = 321;
+    public static final boolean DEF_BOOL = false;
+    public static final String DEF_STR = "default";
+    // Values
+    public static final int VAL_INT = 123;
+    public static final boolean VAL_BOOL = true;
+    public static final String VAL_STR = "test-str";
     // Randoms
     private static final Random random = new Random();
     public static String randomName() {
