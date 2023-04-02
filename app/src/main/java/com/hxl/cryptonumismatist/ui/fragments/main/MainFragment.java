1069542f69424f83fef8cb3705ef5e66b42f4f9a
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-02 23:00:15 +0400

    feat(app): add main fragment for navigating within main screens

---
+++ app/src/main/java/com/hxl/cryptonumismatist/ui/fragments/main/MainFragment.java
@@ -0,0 +1,23 @@
+package com.hxl.cryptonumismatist.ui.fragments.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.hxl.cryptonumismatist.R;
+import com.hxl.cryptonumismatist.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+    FragmentMainBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
