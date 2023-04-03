7f77c41be1cf3da6d56ebc3cb4434058d134b523
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-04 01:09:56 +0400

    fix(welcome/test): remove errors & add function argument to hilt fragment scenario

--- app/src/androidTest/java/com/hxl/cryptonumismatist/util/HiltFragmentScenario.java
+++ app/src/androidTest/java/com/hxl/cryptonumismatist/util/HiltFragmentScenario.java
@@ -1,20 +1,25 @@
-package com.hxl.cryptonumismatist.conf;
+package com.hxl.cryptonumismatist.util;
 
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 
 import com.hxl.cryptonumismatist.HiltTestActivity;
 
 import java.util.Objects;
+import java.util.function.Function;
 
-public final class HiltExt {
-    public static <T extends Fragment> void launchFragmentInHiltContainer(Class<T> fragmentClass, Bundle args, int themeId) {
+public final class HiltFragmentScenario {
+    public static <T extends Fragment> void launchFragmentInHiltContainer(
+            Class<T> fragmentClass,
+            Bundle args,
+            int themeId,
+            Function<Fragment, Void> function
+    ) {
         Intent startActivityIntent = Intent.makeMainActivity(
                 new ComponentName(ApplicationProvider.getApplicationContext(), HiltTestActivity.class)
         );
@@ -35,11 +40,9 @@ public final class HiltExt {
                             .beginTransaction()
                             .add(android.R.id.content, fragment, "")
                             .commitNow();
+
+                    function.apply(fragment);
                 });
 
-    }
-
-    public static <T extends Fragment> void launchFragmentInHiltContainer(Class<T> fragmentClass, Bundle args) {
-        launchFragmentInHiltContainer(fragmentClass, args, androidx.appcompat.R.style.Theme_AppCompat_Empty);
     }
 }
