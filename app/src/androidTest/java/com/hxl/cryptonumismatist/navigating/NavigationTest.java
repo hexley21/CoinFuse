f1f80a25699b8674c30f9920f644dfe4745f9bcf
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-06 13:36:53 +0400

    refactor(app/test): refactor navigation test unit
    Rename from `NavigationFragmentTest` -> `NavigationTest`

--- app/src/androidTest/java/com/hxl/cryptonumismatist/navigating/NavigationTest.java
+++ app/src/androidTest/java/com/hxl/cryptonumismatist/navigating/NavigationTest.java
@@ -1,4 +1,4 @@
-package com.hxl.cryptonumismatist.navigation;
+package com.hxl.cryptonumismatist.navigating;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
@@ -32,7 +32,7 @@ import dagger.hilt.android.testing.HiltAndroidTest;
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 @HiltAndroidTest
-public class NavigationFragmentTest {
+public class NavigationTest {
 
     @Rule(order = 0)
     public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
@@ -57,21 +57,26 @@ public class NavigationFragmentTest {
 
     @Test
     public void testAppNavigation() {
+        // App starts with welcome fragment
         assertEquals(rootNavController.getCurrentDestination().getId(), R.id.welcomeFragment);
 
+        // app navigates to navigation fragment with coins menu fragment open
         onView(withId(R.id.tv_skip)).perform(ViewActions.click());
         scenarioRule.getScenario().onActivity(activity -> mainNavController = Navigation.findNavController(activity.findViewById(R.id.fragment_main_container)));
         assertEquals(rootNavController.getCurrentDestination().getId(), R.id.navigationFragment);
         assertEquals(mainNavController.getCurrentDestination().getId(), R.id.coinsFragment);
 
+        // coins menu fragment navigates to coin details fragment
         onView(withId(R.id.rv_coins)).perform(actionOnItemAtPosition(0, ViewActions.click()));
         assertEquals(rootNavController.getCurrentDestination().getId(), R.id.coinDetailsFragment);
         assertEquals(mainNavController.getCurrentDestination().getId(), R.id.coinsFragment);
 
+        // coin details fragment navigates to navigation fragment with coins menu fragment open
         pressBack();
         assertEquals(rootNavController.getCurrentDestination().getId(), R.id.navigationFragment);
         assertEquals(mainNavController.getCurrentDestination().getId(), R.id.coinsFragment);
 
+        // navigation fragment closes app on back press and invokes activity recreation
         pressBackUnconditionally();
         assertEquals(Lifecycle.State.CREATED, scenarioRule.getScenario().getState());
     }
