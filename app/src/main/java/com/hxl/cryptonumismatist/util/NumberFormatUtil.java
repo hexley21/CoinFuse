cbd13928b590926a8b571096d5e577f2e80d1e88
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-13 18:20:36 +0400

    feat(ui): add number format util

---
+++ app/src/main/java/com/hxl/cryptonumismatist/util/NumberFormatUtil.java
@@ -0,0 +1,36 @@
+package com.hxl.cryptonumismatist.util;
+
+import java.text.DecimalFormat;
+
+public final class NumberFormatUtil {
+
+    public static String formatDouble(Double num, int bigFractions, int smallFractions) {
+        if (num != null) {
+            DecimalFormat df = new DecimalFormat("#");
+            df.setMinimumFractionDigits(bigFractions);
+            if (num > 1.0d) {
+                df.setMaximumFractionDigits(bigFractions);
+                return df.format(num);
+            }
+            df.setMaximumFractionDigits(smallFractions);
+            return df.format(num);
+        }
+        return "-";
+    }
+
+    public static String formatDouble(Double num) {
+        return formatDouble(num, 2, 6);
+    }
+
+    public static String formatDoubleDetailed(Double num) {
+        return formatDouble(num, 2, 10);
+    }
+
+    public static String formatFloat(Float num) {
+        if (num != null) {
+            DecimalFormat df = new DecimalFormat("#.##");
+            return df.format(num);
+        }
+        return "-";
+    }
+}
