db33d8bcf00b0b27a97c0af21d803b136e9ca975
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-17 02:14:08 +0400

    feat(ui/price-history): add basic price history data presentation

--- app/src/main/java/com/hxl/cryptonumismatist/ui/fragments/coins/details/CoinDetailsFragment.java
+++ app/src/main/java/com/hxl/cryptonumismatist/ui/fragments/coins/details/CoinDetailsFragment.java
@@ -15,12 +15,17 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.RequestManager;
+import com.github.mikephil.charting.data.Entry;
 import com.hxl.cryptonumismatist.R;
 import com.hxl.cryptonumismatist.base.BaseFragment;
 import com.hxl.cryptonumismatist.databinding.FragmentCoinDetailsBinding;
 import com.hxl.cryptonumismatist.util.EspressoIdlingResource;
 import com.hxl.domain.model.Coin;
+import com.hxl.domain.model.History;
 import com.hxl.presentation.viewmodels.CoinDetailsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -50,10 +55,13 @@ public class CoinDetailsFragment extends BaseFragment<FragmentCoinDetailsBinding
     @Inject
     RequestManager glide;
 
+    LineChartUtil chartUtil;
+
     @Override
     protected void onCreateView(Bundle savedInstanceState) {
         if (getArguments() != null) {
             coinId = getArguments().getString(coinArg);
+            initChartUtil();
             bind();
         }
     }
@@ -149,6 +157,7 @@ public class CoinDetailsFragment extends BaseFragment<FragmentCoinDetailsBinding
                     @Override
                     public void onSuccess(@NonNull Boolean aBoolean) {
                         binding.cbBookmark.setChecked(aBoolean);
+                        Log.d(TAG, "isCoinBookmarked.onSuccess: bookmark state was checked successfully");
                     }
 
                     @Override
@@ -156,5 +165,53 @@ public class CoinDetailsFragment extends BaseFragment<FragmentCoinDetailsBinding
                         Log.e(TAG, String.format("isCoinBookmarked.onError: %s couldn't be checked", coinId), e);
                     }
                 });
+        chartUtil.drawLineGraph();
+
+        setPriceChart(History.Interval.D1);
+    }
+
+    public void setPriceChart(History.Interval interval) {
+        vm.getCoinHistory(coinId, interval)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<History>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) { }
+
+                    @Override
+                    public void onSuccess(@NonNull List<History> histories) {
+                        ArrayList<Entry> entries = new ArrayList<>();
+                        for (int i = 0; i < histories.size(); i++) {
+                            entries.add(new Entry(histories.get(i).time, histories.get(i).priceUsd.floatValue()));
+                        }
+
+                        chartUtil.setData(entries);
+
+                        if (interval == History.Interval.D1) {
+                            binding.setDayLow(formatFloat(binding.lineChart.getYChartMin()));
+                            binding.setDayHigh(formatFloat(binding.lineChart.getYChartMax()));
+                        }
+                        Log.d(TAG, "setPriceChart.onSuccess: coin price history was gathered successfully");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.e(
+                                TAG,
+                                String.format("setPriceChart.onError: couldn't get price history of %s, with interval %s", coinId, interval.param),
+                                e
+                        );
+                    }
+                });
+
+    }
+
+    private void initChartUtil() {
+        TypedValue chartColor = new TypedValue();
+        TypedValue textColor = new TypedValue();
+        Resources.Theme theme = requireContext().getTheme();
+        theme.resolveAttribute(com.google.android.material.R.attr.colorPrimary, chartColor, true);
+        theme.resolveAttribute(com.google.android.material.R.attr.colorOnSurface, textColor, true);
+
+        chartUtil = new LineChartUtil(binding.lineChart, chartColor.data, textColor.data);
     }
 }
