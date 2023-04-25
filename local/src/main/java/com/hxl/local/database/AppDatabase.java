1ed1b5bc415b695952e259cdbad71ed0f5878ed1
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-25 23:30:23 +0400

    feat(search-history): add coin-search-entity to app database

--- local/src/main/java/com/hxl/local/database/AppDatabase.java
+++ local/src/main/java/com/hxl/local/database/AppDatabase.java
@@ -5,11 +5,13 @@ import androidx.room.RoomDatabase;
 
 import com.hxl.local.database.coin.BookmarkDao;
 import com.hxl.local.database.coin.CoinDao;
+import com.hxl.local.database.coin.CoinSearchDao;
 import com.hxl.local.model.coin.BookmarkEntity;
 import com.hxl.local.model.coin.CoinEntity;
+import com.hxl.local.model.coin.CoinSearchEntity;
 
 @Database(
-        entities = {CoinEntity.class, BookmarkEntity.class},
+        entities = {CoinEntity.class, BookmarkEntity.class, CoinSearchEntity.class},
         version = 1,
         exportSchema = false
 )
@@ -21,4 +23,5 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract CoinDao coinDao();
     public abstract BookmarkDao bookmarkDao();
+    public abstract CoinSearchDao coinSearchDao();
 }
