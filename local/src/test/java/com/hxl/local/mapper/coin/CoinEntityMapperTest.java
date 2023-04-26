b8b7ad5baa9e0d8ab31a0824eb0af09a606b8259
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-26 11:27:11 +0400

    test(search-history): add missing android-junit4 runner class

--- local/src/test/java/com/hxl/local/mapper/coin/CoinEntityMapperTest.java
+++ local/src/test/java/com/hxl/local/mapper/coin/CoinEntityMapperTest.java
@@ -3,11 +3,15 @@ package com.hxl.local.mapper.coin;
 import static com.hxl.local.fake.LocalTestConstants.*;
 import static org.junit.Assert.assertEquals;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import com.hxl.domain.model.Coin;
 import com.hxl.local.model.coin.CoinEntity;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class CoinEntityMapperTest {
 
     // Arrange
