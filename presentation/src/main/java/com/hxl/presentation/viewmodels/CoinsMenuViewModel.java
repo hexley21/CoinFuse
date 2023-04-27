9f3ddaa8703421be5db7bfbb52ae269a3a8173d0
hexley21 <a.kupreishvili.21@gmail.com>
2023-04-27 19:46:06 +0400

    feat(search-history): add use-cases to coins menu view-model

--- presentation/src/main/java/com/hxl/presentation/viewmodels/CoinsMenuViewModel.java
+++ presentation/src/main/java/com/hxl/presentation/viewmodels/CoinsMenuViewModel.java
@@ -1,19 +1,21 @@
 package com.hxl.presentation.viewmodels;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import com.hxl.domain.interactors.coins.DeleteCoinSearchQuery;
+import com.hxl.domain.interactors.coins.GetCoinSearchHistory;
 import com.hxl.domain.interactors.coins.GetCoins;
+import com.hxl.domain.interactors.coins.InsertCoinSearchQuery;
 import com.hxl.domain.interactors.coins.SearchCoins;
 import com.hxl.domain.model.Coin;
+import com.hxl.domain.model.SearchQuery;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-import javax.inject.Inject;
-
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
 
 @HiltViewModel
@@ -21,6 +23,9 @@ public class CoinsMenuViewModel extends ViewModel {
 
     @NotNull private final GetCoins getCoins;
     @NotNull private final SearchCoins searchCoins;
+    @NotNull private final GetCoinSearchHistory getCoinSearchHistory;
+    @NotNull private final InsertCoinSearchQuery insertCoinSearchQuery;
+    @NotNull private final DeleteCoinSearchQuery deleteCoinSearchQuery;
 
     public Single<List<Coin>> getCoins() {
         return getCoins.invoke();
@@ -30,9 +35,27 @@ public class CoinsMenuViewModel extends ViewModel {
         return searchCoins.invoke(key);
     }
 
-    @Inject
-    public CoinsMenuViewModel(@NonNull GetCoins getCoins, @NonNull SearchCoins searchCoins) {
+    public Single<List<SearchQuery>> getCoinSearchHistory() {
+        return getCoinSearchHistory.invoke();
+    }
+
+    public Completable insertCoinSearchQuery(String query) {
+        return insertCoinSearchQuery.invoke(query);
+    }
+
+    public Completable deleteCoinSearchQuery(String query) {
+        return deleteCoinSearchQuery.invoke(query);
+    }
+
+    public CoinsMenuViewModel(@NotNull GetCoins getCoins,
+                              @NotNull SearchCoins searchCoins,
+                              @NotNull GetCoinSearchHistory getCoinSearchHistory,
+                              @NotNull InsertCoinSearchQuery insertCoinSearchQuery,
+                              @NotNull DeleteCoinSearchQuery deleteCoinSearchQuery) {
         this.getCoins = getCoins;
         this.searchCoins = searchCoins;
+        this.getCoinSearchHistory = getCoinSearchHistory;
+        this.insertCoinSearchQuery = insertCoinSearchQuery;
+        this.deleteCoinSearchQuery = deleteCoinSearchQuery;
     }
 }
