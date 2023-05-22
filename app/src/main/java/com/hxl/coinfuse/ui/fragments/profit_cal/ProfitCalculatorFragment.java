c29f38c06c000c58abfeec5ee2e34765c295daf7
hexley21 <a.kupreishvili.21@gmail.com>
2023-05-22 21:18:47 +0400

    feat(profit-calculator): add profit calculation

--- app/src/main/java/com/hxl/coinfuse/ui/fragments/profit_cal/ProfitCalculatorFragment.java
+++ app/src/main/java/com/hxl/coinfuse/ui/fragments/profit_cal/ProfitCalculatorFragment.java
@@ -1,12 +1,23 @@
 package com.hxl.coinfuse.ui.fragments.profit_cal;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.hxl.coinfuse.R;
 import com.hxl.coinfuse.base.BaseFragment;
 import com.hxl.coinfuse.databinding.FragmentProfitCalculatorBinding;
+import com.hxl.coinfuse.util.UiUtils;
 import com.hxl.presentation.viewmodels.ProfitCalculatorViewModel;
+
+import java.util.function.Consumer;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -22,9 +33,113 @@ public class ProfitCalculatorFragment extends BaseFragment<FragmentProfitCalcula
         return ProfitCalculatorViewModel.class;
     }
 
+    private static final String TAG = "ProfitCalculatorFr";
+
     @Override
     protected void onCreateView(Bundle savedInstanceState) {
         super.onCreateView(savedInstanceState);
         binding.setCurrency("$");
+        if (!vm.getCurrentProfit().hasObservers()) {
+            vm.getCurrentProfit().observe(requireActivity(), val -> {
+                binding.setProfit(val);
+                if (val > 0) {
+                    binding.profitResult.setTextColor(UiUtils.getColor(requireContext(), R.attr.growth));
+                }
+                else if (val < 0){
+                    binding.profitResult.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorError));
+                }
+                else {
+                    binding.profitResult.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorOnSurface));
+                }
+            });
+        }
+        if (!vm.getCurrentInvestment().hasObservers()) {
+            vm.getCurrentInvestment().observe(requireActivity(), val -> {
+                binding.setInvestment(val);
+                if (val > 0) {
+                    binding.profitInvestment.setTextColor(UiUtils.getColor(requireContext(), R.attr.growth));
+                }
+                else if (val < 0){
+                    binding.profitInvestment.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorError));
+                }
+                else {
+                    binding.profitInvestment.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorOnSurface));
+                }
+            });
+
+        }
+
+        if (!vm.getCurrentExit().hasObservers()) {
+            vm.getCurrentExit().observe(requireActivity(), val -> {
+                binding.setExit(val);
+                if (val > 0) {
+                    binding.profitExit.setTextColor(UiUtils.getColor(requireContext(), R.attr.growth));
+                }
+                else if (val < 0){
+                    binding.profitExit.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorError));
+                }
+                else {
+                    binding.profitExit.setTextColor(UiUtils.getColor(requireContext(), com.google.android.material.R.attr.colorOnSurface));
+                }
+            });
+        }
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        assert binding.tfProfitInvestment.getEditText() != null;
+        assert binding.tfProfitSell.getEditText() != null;
+        assert binding.tfProfitInFee.getEditText() != null;
+        assert binding.tfProfitExFee.getEditText() != null;
+        assert binding.tfProfitBuy.getEditText() != null;
+
+        binding.profitToolbar.setNavigationOnClickListener(v -> {
+            binding.tfProfitBuy.getEditText().setText("");
+            binding.tfProfitInvestment.getEditText().setText("");
+            binding.tfProfitSell.getEditText().setText("");
+            binding.tfProfitInFee.getEditText().setText("");
+            binding.tfProfitExFee.getEditText().setText("");
+            vm.clearEverything();
+        });
+
+        binding.tfProfitBuy.getEditText().addTextChangedListener(createTextWatcher(val -> vm.setBuyPriceField(val)));
+        binding.tfProfitInvestment.getEditText().addTextChangedListener(createTextWatcher(val -> vm.setInvestmentField(val)));
+        binding.tfProfitSell.getEditText().addTextChangedListener(createTextWatcher(val -> vm.setSellPriceField(val)));
+        binding.tfProfitInFee.getEditText().addTextChangedListener(createTextWatcher(val -> vm.setInvestmentFeeField(val)));
+        binding.tfProfitExFee.getEditText().addTextChangedListener(createTextWatcher(val -> vm.setExitFeeField(val)));
+
+        binding.tfProfitBuy.addOnEndIconChangedListener((textInputLayout, previousIcon) -> vm.setBuyPriceField(0.0D));
+        binding.tfProfitInvestment.addOnEndIconChangedListener((textInputLayout, previousIcon) -> vm.setInvestmentField(0.0D));
+        binding.tfProfitSell.addOnEndIconChangedListener((textInputLayout, previousIcon) -> vm.setSellPriceField(0.0D));
+        binding.tfProfitInFee.addOnEndIconChangedListener((textInputLayout, previousIcon) -> vm.setInvestmentFeeField(0.0D));
+        binding.tfProfitExFee.addOnEndIconChangedListener((textInputLayout, previousIcon) -> vm.setExitFeeField(0.0D));
+    }
+
+
+    private TextWatcher createTextWatcher(Consumer<Double> val) {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 0) {
+                    val.accept(0.0);
+                    return;
+                }
+                try {
+                    val.accept(Double.parseDouble(s.toString()));
+                }
+                catch (Exception e) {
+                    val.accept(0.0);
+                    Log.e(TAG, "onTextChanged: failed", e);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        };
+    }
+
 }
--- presentation/src/main/java/com/hxl/presentation/viewmodels/ProfitCalculatorViewModel.java
+++ presentation/src/main/java/com/hxl/presentation/viewmodels/ProfitCalculatorViewModel.java
@@ -1,17 +1,28 @@
 package com.hxl.presentation.viewmodels;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.hxl.domain.interactors.coins.SearchCoins;
+import com.hxl.domain.model.Coin;
+import com.hxl.presentation.livedata.StateLiveData;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 @HiltViewModel
 public class ProfitCalculatorViewModel extends ViewModel {
 
+    private static final String TAG = "ProfitCalculatorVM";
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     @NonNull private final SearchCoins searchCoins;
 
     @Inject
@@ -19,4 +30,142 @@ public class ProfitCalculatorViewModel extends ViewModel {
         this.searchCoins = searchCoins;
     }
 
+    private StateLiveData<List<Coin>> currentSearch;
+    private MutableLiveData<Double> currentProfit;
+    private MutableLiveData<Double> currentInvestment;
+    private MutableLiveData<Double> currentExit;
+
+    private Double buyPrice;
+    private Double investment;
+    private Double sellPrice;
+    private Double investmentFee;
+    private Double exitFee;
+
+    public void setBuyPriceField(Double buyPrice) {
+        this.buyPrice = buyPrice;
+        calculateProfit();
+    }
+
+    public void setInvestmentField(Double investment) {
+        this.investment = investment;
+        calculateProfit();
+    }
+
+    public void setSellPriceField(Double sellPrice) {
+        this.sellPrice = sellPrice;
+        calculateProfit();
+    }
+
+    public void setInvestmentFeeField(Double investmentFee) {
+        this.investmentFee = investmentFee;
+        calculateProfit();
+    }
+
+    public void setExitFeeField(Double exitFee) {
+        this.exitFee = exitFee;
+        calculateProfit();
+    }
+
+    public StateLiveData<List<Coin>> getCurrentSearch() {
+        if (currentSearch == null) {
+            currentSearch = new StateLiveData<>();
+        }
+
+        return currentSearch;
+    }
+
+    public MutableLiveData<Double> getCurrentProfit() {
+        if (currentProfit == null) {
+            currentProfit = new MutableLiveData<>();
+        }
+
+        return currentProfit;
+    }
+
+    public MutableLiveData<Double> getCurrentInvestment() {
+        if (currentInvestment == null) {
+            currentInvestment = new MutableLiveData<>();
+        }
+
+        return currentInvestment;
+    }
+
+    public MutableLiveData<Double> getCurrentExit() {
+        if (currentExit == null) {
+            currentExit = new MutableLiveData<>();
+        }
+
+        return currentExit;
+    }
+
+    public void fetchSearch(String query) {
+        getCurrentSearch().setLoading();
+        searchCoins.invoke(query)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        search -> {
+                            getCurrentSearch().setSuccess(search);
+                            Log.d(TAG, "fetchSearch: success");
+                        },
+                         e -> {
+                            getCurrentSearch().setError(e);
+                             Log.e(TAG, "fetchSearch: failed", e);
+                         },
+                        compositeDisposable
+                );
+    }
+
+    public void setProfit(double val) {
+        getCurrentProfit().setValue(val);
+    }
+
+    public void setInvestment(double val) {
+        getCurrentInvestment().setValue(val);
+    }
+
+    public void setExit(double val) {
+        getCurrentExit().setValue(val);
+    }
+
+    public void calculateProfit() {
+        if (buyPrice == null || buyPrice == 0.0D) {
+            setProfit(0.0D);
+            return;
+        }
+        if (investment == null || investment == 0.0D) {
+            setProfit(0.0D);
+            return;
+        }
+        if (sellPrice == null || sellPrice == 0.0D) {
+            setProfit(0.0D);
+            return;
+        }
+        final double nnInvestmentFee = investmentFee == null ? 0.0 : investmentFee;
+        final double nnExitFee = exitFee == null ? 0.0 : exitFee;
+
+        final double profit = ((sellPrice - buyPrice) * (investment / buyPrice)) - (nnInvestmentFee + nnExitFee);
+
+        setProfit(profit);
+        setInvestment(investment + nnInvestmentFee);
+        setExit(investment + nnInvestmentFee + profit);
+    }
+
+    public void clearEverything() {
+        buyPrice = 0.0D;
+        investment = 0.0D;
+        sellPrice = 0.0D;
+        investmentFee = 0.0D;
+        exitFee = 0.0D;
+        setProfit(0.0D);
+        setInvestment(0.0D);
+        setExit(0.0D);
+    }
+
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
+        Log.d(TAG, "onCleared: CompositeDisposable was disposed");
+    }
 }
