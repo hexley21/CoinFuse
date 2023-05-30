5af073f63f3d7127e8e2065fa3fc8b8a4c2dbcc6
Hexley21 <a.kupreishvili.21@gmail.com>
2023-05-30 18:32:26 +0400

    test(domain): add tests for erase-bookmark & erase-coin-cache

---
+++ domain/src/test/java/com/hxl/interactors/coins/EraseBookmarksTest.java
@@ -0,0 +1,38 @@
+package com.hxl.interactors.coins;
+
+import static org.mockito.Mockito.when;
+
+import com.hxl.domain.interactors.coins.EraseBookmarks;
+import com.hxl.domain.repository.CoinRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.inject.Inject;
+
+import io.reactivex.rxjava3.core.Completable;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EraseBookmarksTest {
+
+    @Mock
+    private CoinRepository repository;
+
+    @InjectMocks
+    private EraseBookmarks interactor;
+
+    @Test
+    public void invokeReturnsCompletableFromRepository() {
+        // Act
+        when(repository.eraseBookmarks()).thenReturn(Completable.complete());
+        // Assert
+        interactor.invoke().test()
+                .awaitCount(1)
+                .assertComplete()
+                .assertNoErrors();
+    }
+
+}
---
+++ domain/src/test/java/com/hxl/interactors/coins/EraseCoinCacheTest.java
@@ -0,0 +1,37 @@
+package com.hxl.interactors.coins;
+
+import static org.mockito.Mockito.when;
+
+import com.hxl.domain.interactors.coins.EraseCoinCache;
+import com.hxl.domain.repository.CoinRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.inject.Inject;
+
+import io.reactivex.rxjava3.core.Completable;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EraseCoinCacheTest {
+
+    @Mock
+    private CoinRepository repository;
+    @InjectMocks
+    private EraseCoinCache interactor;
+
+    @Test
+    public void invokeReturnsCompletableFromRepository() {
+        // Act
+        when(repository.eraseCoinCache()).thenReturn(Completable.complete());
+        // Assert
+        interactor.invoke().test()
+                .awaitCount(1)
+                .assertComplete()
+                .assertNoErrors();
+    }
+
+}
