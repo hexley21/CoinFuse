4d7f6bd47012df144f798f486fac71472bc3a441
Hexley21 <a.kupreishvili.21@gmail.com>
2023-06-01 14:05:57 +0400

    test: remove unused stuff from welcome-fragment test
    Remove navigation test.

--- app/src/androidTest/java/com/hxl/coinfuse/welcome/WelcomeFragmentTest.java
+++ app/src/androidTest/java/com/hxl/coinfuse/welcome/WelcomeFragmentTest.java
@@ -1,24 +1,16 @@
 package com.hxl.coinfuse.welcome;
 
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.hxl.coinfuse.util.HiltFragmentScenario.launchFragmentInHiltContainer;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
-import androidx.navigation.Navigation;
-import androidx.navigation.testing.TestNavHostController;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
-import com.hxl.coinfuse.HiltTestActivity;
 import com.hxl.coinfuse.R;
 import com.hxl.coinfuse.ui.fragments.welcome.WelcomeFragment;
 import com.hxl.domain.interactors.prefs.GetWelcome;
@@ -44,24 +36,16 @@ public class WelcomeFragmentTest {
     @Inject
     GetWelcome getWelcome;
 
-    TestNavHostController navController;
-
     @Before
     public void setUp() {
         hiltRule.inject();
-        navController = new TestNavHostController(ApplicationProvider.getApplicationContext());
     }
 
     @Test
     @SuppressWarnings("ConstantConditions")
     public void fragmentBehavesAsExpected() {
         // Act
-        ActivityScenario<HiltTestActivity> scenario = launchFragmentInHiltContainer(WelcomeFragment.class);
-        scenario.onActivity(activity -> {
-            navController.setGraph(R.navigation.nav_root);
-            navController.setCurrentDestination(R.id.welcomeFragment);
-            Navigation.setViewNavController(activity.requireViewById(android.R.id.content), navController);
-        });
+        launchFragmentInHiltContainer(WelcomeFragment.class);
         // Assert
         onView(withId(R.id.walk_through_pager)).check(matches(isDisplayed()));
 
@@ -75,10 +59,6 @@ public class WelcomeFragmentTest {
         onView(withId(R.id.btn_next)).check(matches(not(isDisplayed())));
         onView(withId(R.id.tv_skip)).check(matches(not(isDisplayed())));
         onView(withId(R.id.btn_get_started)).check(matches(isDisplayed()));
-        onView(withId(R.id.btn_get_started)).perform(ViewActions.click());
-
-        assertEquals(navController.getCurrentDestination().getId(), R.id.navigationFragment);
-        assertFalse(getWelcome.invoke());
     }
 
 }
