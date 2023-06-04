7752fc7a3521c67b39a4f3e29aab45f6460f394a
Hexley21 <a.kupreishvili.21@gmail.com>
2023-06-04 23:53:32 +0400

    fix: resolve gradle static log mocking issue

--- presentation/src/test/java/com/hxl/presentation/ViewModelSetup.java
+++ presentation/src/test/java/com/hxl/presentation/ViewModelSetup.java
@@ -1,13 +1,5 @@
 package com.hxl.presentation;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mockStatic;
-
-import android.annotation.SuppressLint;
-import android.util.Log;
-
 import org.junit.rules.ExternalResource;
 
 import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins;
@@ -17,19 +9,8 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ViewModelSetup extends ExternalResource {
 
-    @SuppressLint("CheckResult")
     @Override
     protected void before() {
-        mockStatic(Log.class);
-        doAnswer(invocation -> {
-            String tag = invocation.getArgument(0);
-            String message = invocation.getArgument(1);
-            Throwable throwable = invocation.getArgument(2);
-            System.out.println(tag + ": " + message);
-            throwable.printStackTrace();
-            return 0;
-        }).when(Log.class);
-        Log.e(anyString(), anyString(), any(Throwable.class));
 
         Scheduler immediateScheduler = Schedulers.trampoline();
 
